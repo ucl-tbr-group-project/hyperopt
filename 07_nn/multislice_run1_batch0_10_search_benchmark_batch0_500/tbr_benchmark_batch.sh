fd63233c81d958f103bab5e44fb2275afa42cc9c
Petr Manek <petrmanek@users.noreply.github.com>
2020-04-25 20:30:10 +0100

    add nn and rbf multislice benchmarks

---
+++ 07_nn/multislice_run1_batch0_10_search_benchmark_batch0_500/tbr_benchmark_batch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p RCIF
+#SBATCH -N 1
+#SBATCH -c 40
+#SBATCH --time=1-23:59:59
+#SBATCH -o /share/rcifdata/pmanek/fusion/hyperopt/07_nn/multislice_run1_batch0_10_search_benchmark_batch0_500/benchmark.log
+#SBATCH -J 07_nn/multislice_run1_batch0_10_search_benchmark_batch0_500
+
+export OMP_NUM_THREADS=$SLURM_JOB_CPUS_PER_NODE
+export MAIN_DIR=/share/rcifdata/pmanek/fusion
+
+srun -n1 ${MAIN_DIR}/hyperopt/search_template/tbr_benchmark_run.sh 07_nn/multislice_run1_batch0_10_search_benchmark_batch0_500
