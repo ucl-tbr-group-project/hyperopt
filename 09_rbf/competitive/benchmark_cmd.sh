d5c00c6545b5486b7dc275dd27d541c87f27acf3
Petr Manek <petrmanek@users.noreply.github.com>
2020-04-27 10:33:28 +0100

    add rbf competitive models

--- 09_rbf/competitive/benchmark_cmd.sh
+++ 09_rbf/competitive/benchmark_cmd.sh
@@ -6,4 +6,4 @@ OUT_DIR=/share/rcifdata/pmanek/fusion/hyperopt/09_rbf/competitive/best_${INPUT_S
 rm -rf ${OUT_DIR}
 mkdir -p ${OUT_DIR}
 
-tbr_search_benchmark /share/rcifdata/pmanek/fusion/data/run1 0 500 /share/rcifdata/pmanek/fusion/hyperopt/09_rbf/multislice_run1_batch0_10_search/search.csv ${OUT_DIR} rbf --feature-def=/share/rcifdata/pmanek/fusion/regression/features/all --n-jobs=1 --n-best-models=5 --set-size=${INPUT_SIZE}000
+tbr_search_benchmark /share/rcifdata/pmanek/fusion/data/run1 0 500 /share/rcifdata/pmanek/fusion/hyperopt/09_rbf/multislice_run1_batch0_10_search/search.csv ${OUT_DIR} rbf --feature-def=/share/rcifdata/pmanek/fusion/regression/features/all --n-jobs=1 --n-best-models=5 --save-plots --set-size=${INPUT_SIZE}000
