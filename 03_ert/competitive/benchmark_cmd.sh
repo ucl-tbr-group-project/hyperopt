04f4e3e3a01a6bae7733e9a8a405671112619621
Petr Manek <petrmanek@users.noreply.github.com>
2020-04-27 10:34:25 +0100

    add competitive ert models

--- 03_ert/competitive/benchmark_cmd.sh
+++ 03_ert/competitive/benchmark_cmd.sh
@@ -6,4 +6,4 @@ OUT_DIR=/share/rcifdata/pmanek/fusion/hyperopt/03_ert/competitive/best_${INPUT_S
 rm -rf ${OUT_DIR}
 mkdir -p ${OUT_DIR}
 
-tbr_search_benchmark /share/rcifdata/pmanek/fusion/data/run1 0 500 /share/rcifdata/pmanek/fusion/hyperopt/03_ert/multislice_run1_batch0_10_search/search.csv ${OUT_DIR} ert --feature-def=/share/rcifdata/pmanek/fusion/regression/features/all --n-jobs=1 --n-best-models=5 --set-size=${INPUT_SIZE}000
+tbr_search_benchmark /share/rcifdata/pmanek/fusion/data/run1 0 500 /share/rcifdata/pmanek/fusion/hyperopt/03_ert/multislice_run1_batch0_10_search/search.csv ${OUT_DIR} ert --feature-def=/share/rcifdata/pmanek/fusion/regression/features/all --save-plots --n-jobs=1 --n-best-models=5 --set-size=${INPUT_SIZE}000
