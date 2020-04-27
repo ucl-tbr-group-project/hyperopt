9f580e77e559818303bb172424a15ddf98909d3d
Petr Manek <petrmanek@users.noreply.github.com>
2020-04-27 23:14:21 +0100

    add more nn competitive models

---
+++ 07_nn/competitive/benchmark_all_archs.sh
@@ -0,0 +1,9 @@
+SCRIPT=benchmark_cmd_arb_arch_200k.sh
+
+bash ${SCRIPT} 2f_16
+bash ${SCRIPT} 2f_32
+bash ${SCRIPT} 2f_64
+bash ${SCRIPT} 4f_16
+bash ${SCRIPT} 4f_32
+bash ${SCRIPT} 4f_64
+bash ${SCRIPT} 4f_128
